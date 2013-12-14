36b3f3ece236d42f4f5b1ea7fbad18f6f491d0c3
Francesco Ciocchetti <primeroznl@gmail.com>
2013-12-14 16:21:33 +0000

    Fix hosts class to run when parameters are ok - again

--- modules/base/manifests/hosts.pp
+++ modules/base/manifests/hosts.pp
@@ -6,7 +6,8 @@ class base::hosts (
     ensure => 'present',
   }
 
-  if ($base::hosts::hosts != '') and validate_hash($base::hosts::hosts) {
+  if ($base::hosts::hosts != '') {
+    validate_hash($base::hosts::hosts) 
     create_resources(host,$hosts,$hosts_default)
   }
 
