1e766b155a16cadf30cae14ec6b7a1ceee2617ba
Francesco Ciocchetti <primeroznl@gmail.com>
2013-12-14 16:28:19 +0000

    Add creation of SELF entry in /etc/hosts files

--- modules/base/manifests/hosts.pp
+++ modules/base/manifests/hosts.pp
@@ -1,14 +1,24 @@
 class base::hosts (
   $hosts    =   '',
+  $self     = true,
   ) {
 
   $hosts_default = {
     ensure => 'present',
   }
 
+  #Add entries from HIERA or passed parameters
   if ($base::hosts::hosts != '') {
     validate_hash($base::hosts::hosts) 
     create_resources(host,$hosts,$hosts_default)
   }
 
+  #Add Entry for host itself
+  if ($base::hosts::self) {
+    host { ${::hostname}:
+      ip = ${::ipaddress},
+      host_aliases = ${::fqdn},
+      ensure = 'present',
+    }
+  }
 }
