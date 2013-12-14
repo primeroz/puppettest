f8cf37683f202c851b9c75ef12370be58d46a803
Francesco Ciocchetti <primeroznl@gmail.com>
2013-12-14 16:38:22 +0000

    fix some facts to work use namespacing for all variables fix some typos in the resource

--- modules/base/manifests/hosts.pp
+++ modules/base/manifests/hosts.pp
@@ -10,15 +10,15 @@ class base::hosts (
   #Add entries from HIERA or passed parameters
   if ($base::hosts::hosts != '') {
     validate_hash($base::hosts::hosts) 
-    create_resources(host,$hosts,$hosts_default)
+    create_resources(host,$base::hosts::hosts,$base::hosts::hosts_default)
   }
 
   #Add Entry for host itself
   if ($base::hosts::self) {
-    host { ${::hostname}:
-      ip = ${::ipaddress},
-      host_aliases = ${::fqdn},
-      ensure = 'present',
+    host { $::hostname:
+      ip            => $::ipaddress,
+      host_aliases  => $::fqdn,
+      ensure        => 'present',
     }
   }
 }
