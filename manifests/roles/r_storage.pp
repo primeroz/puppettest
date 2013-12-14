4b110ffb70499952f248eda4eece01c5690dbcd7
Francesco Ciocchetti <primeroznl@gmail.com>
2013-12-14 21:31:38 +0000

    Remove zfs creation from role, it has moved to zol::resources

--- manifests/roles/r_storage.pp
+++ manifests/roles/r_storage.pp
@@ -3,17 +3,6 @@ class r_storage {
 
     class { 'zol': }
 
-    #    $zolyaml = loadyaml("/etc/puppet/data/nodes/${::fqdn}.yaml")
-    #    $pools_defaults = {
-    #	require => Class['zol'],
-    #    }
-    #    $zfs_defaults = {
-    #	require => Class['zol'],
-    #    }
-    #
-    #    create_resources(zol::zpool,$zolyaml[zpool],$pools_defaults)
-    #    create_resources(zol::zfs,$zolyaml[zfs],$zfs_defaults)
-
     anchor { 'r_storage::begin': }
     anchor { 'r_storage::end': }
 
